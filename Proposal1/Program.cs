b18c6e38e81b1c6b5aafa164c96811222565fb41
aMetini <amberella1981@yahoo.com>
2021-01-13 09:13:49 +0100

    added data and models folders

--- Proposal1/Program.cs
+++ Proposal1/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Proposal1
 {
@@ -6,7 +13,14 @@ namespace Proposal1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var db = new TBMContext())
+            {
+                if (db.Database.CanConnect())
+                {
+                    db.Stores
+                }
+                else Debug.WriteLine("Connection Failed!");
+            }
         }
     }
 }
